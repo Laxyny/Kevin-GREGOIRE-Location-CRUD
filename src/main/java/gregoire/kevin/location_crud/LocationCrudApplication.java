ecf852cc79d7a873794e04cf20f0efbc6b870bd1
Laxyny <kevgreg30@gmail.com>
2024-11-14 09:25:04 +0100

    Initial commit

---
+++ src/main/java/gregoire/kevin/location_crud/LocationCrudApplication.java
@@ -0,0 +1,13 @@
+package gregoire.kevin.location_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gregoire/kevin/location_crud/LocationCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package gregoire.kevin.location_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
